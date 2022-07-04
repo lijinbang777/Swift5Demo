72d9913f05dee63aac239f944322fd312a92a47e
lijinbang777 <jacklee18364166614@gmail.com>
2022-07-04 21:24:37 +0800

    init swift5 project

---
+++ Swift5Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift5Demo
+//
+//  Created by jinbang.li on 2022/7/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
